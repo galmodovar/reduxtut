1987ec78ddbbdbd83cd6402565803966cdabd87c
galmodovar <galmodovar1@gmail.com>
2022-03-05 17:07:52 -0600

    add users

--- src/features/posts/postsList.js
+++ src/features/posts/postsList.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { PostAuthor } from "./PostAuthor";
+import { TimeAgo } from "./TimeAgo";
 
 
 
@@ -9,10 +11,15 @@ export const PostsList = () => {
     
     
     
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
-    const renderedPosts = posts.map(post => (
+    const renderedPosts = orderedPosts.map(post => (
         <article className="post-excerpt" key={post.id}>
             <h3>{post.title}</h3>
+            <div>
+              <PostAuthor userId={post.user} />
+              <TimeAgo timestamp={post.date} />
+            </div>
             <p className="post-content">{post.content.substring(0,100)}</p>
             <Link to={`/posts/${post.id}`} className='button muted-button'>
               View Post
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,15 +1,18 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { sub } from 'date-fns'
 
 const initialState = [ 
     {
         id: '1',
         title: 'First Post!', 
-        content: 'Hello!'
+        content: 'Hello!',
+        date: sub(new Date(), { minutes: 10 }).toISOString()
     },
     {
         id: '2',
         title: 'Second Post!', 
-        content: 'More Text!'
+        content: 'More Text!',
+        date: sub(new Date(), { minutes: 5}).toISOString()
     }
 ]
 
@@ -19,14 +22,16 @@ const postsSlice = createSlice({
     reducers: {
         postAdded: {
             reducer(state, action) {
-            state.push(action.payload)
+                state.push(action.payload)
         },
-        prepare(title, content) {
+        prepare(title, content, userId) {
             return {
                 payload: {
                     id: nanoid(),
+                    date: new Date().toISOString(),
                     title,
-                    content
+                    content,
+                    user: userId
                 }
             }
         }
